d54f9b0436a336498125f9940c5ca177aaf165f5
yamamoto.hiroya <yamamoto.hiroya@hamee.co.jp>
2021-11-28 12:21:19 +0900

    ハイライトと切り替えの処理を実装

--- js/asana_code_highlight.js
+++ js/asana_code_highlight.js
@@ -2,6 +2,9 @@
  * 基本方針
  * 画面が動的変わるので適用が面倒くさい
  * 拡張アイコンをクリックしたタイミングで適用/解除をすることにする
+ *
+ * ハイライトは以下のライブラリを使用
+ * @see https://github.com/highlightjs/highlight.js
  */
 $(function(){
   /**
@@ -20,26 +23,40 @@ $(function(){
     }
 
     /** ----- main ----- **/
+    // TODO: 改行情報も保持したい
+    // TODO: ```php aaa```みたいな構文に対応したい
+    // TODO: autoがあるっぽい？あるなら嬉しい
+
     // タスク説明文があればハイライトする
-    if($(".TaskDescription").length !== 0){
-      console.log('タスク本文要素発見');
-      var task_description = $(".TaskDescription");
-      var task_description_text = $(".TaskDescription").text();
+    var task_description = $(".TaskDescription");
+    if(task_description.find(".ProsemirrorEditor").length !== 0){
+      console.log('タスク本文要素を発見したのでハイライト欄に切り替えます。');
+      // 元の要素を保持しておく
+      task_description_html = $(".TaskDescription").children().clone(true);
+
+      var task_description_text = task_description.text();
       var highlight_block = "<pre><code class='sql' id='highlight'></code></pre>";
+      var highlight_block = "<pre><code class='php' id='highlight'></code></pre>";
+      var highlight_block = "<pre><code class='sh' id='highlight'></code></pre>";
       task_description.empty();
       task_description.append(highlight_block);
       $('#highlight').text(task_description_text);
+
+      // ハイライト処理
       $('#highlight').each(function(i, block) {
         hljs.highlightBlock(block);
       });
 
     // ハイライト欄があれば元に戻す
-    } else if(false) {
-      console.log('ハイライト欄発見');
+    } else if($("#highlight").length !== 0) {
+      // TODO: イベントも含めて元に戻したつもりだが、元に戻すと保存が効かなくなるっぽい？
+      console.log('ハイライト欄を発見したので元の要素に切り替えます。');
+      task_description.empty();
+      task_description.html(task_description_html);
 
     // 対象の要素がなければ何もしない
     } else {
-      console.log("対象の要素がなかったため何もしません");
+      console.log("対象の要素がなかったため何もしません。");
     }
 
     sendResponse('Done');
